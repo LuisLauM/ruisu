570bc9fc41e8ae3e28a6bbe70ef16a3721742e45
Luis Lau <luis.laum@gmail.ccom>
2015-09-18 16:55:33 -0500

    Initialize manual generation using roxygen2.

--- R/centroidAssigner.R
+++ R/centroidAssigner.R
@@ -1,3 +1,19 @@
+#' @name centroidAssigner
+#' @aliases centroidAssigner
+#' @title Returns centroid values from Isoparalitoral-area codes.
+#' 
+#' @description The function takes a vector of isoparalitoral Areas and returns centroids, i.e. latitude and longitude values.
+#' \code{old} parameter is used to indicate the type of shape used: Ancient (elaborated, roughly, 
+#' in 1992; it has errors respect coast line) and New (made in 2012 from a shape of coastline more accurate). 
+#' The variations between the values -about centroid- ranging from a few meters (for nearshore areas) to almost 
+#' half a nautical mile (for those further away from the coast).
+#' 
+#' @usage centroidAssigner(isoCode, old = TRUE)
+#' @param isoCode Vector of isoparalitoral-areas codes.
+#' @param old \code{logical}. Do you want to use "Old" shape of Isoparalitoral Areas?
+#' 
+#' @example areaCodes <- c(1050, 4043, 17073, 27103)
+#' centroidAssigner(isoCode)
 centroidAssigner <- function(isoCode, old = TRUE)
 {
   if(old)
